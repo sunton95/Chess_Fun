ebf704284b6c768ef3e0e96e641d620e7e766af0
prmanager1337 <anton.sundqvist95@gmail.com>
2021-12-08 03:11:47 +0100

    kk

--- kekW.py
+++ kekW.py
@@ -5,8 +5,14 @@ pygame.init()
 screen = pygame.display.set_mode((640, 480))
 texture_pieces = pygame.image.load('Chess_Pieces_Sprite.png')
 
-pawn = pygame.Surface((200,200))
-pawn.blit(texture_pieces, (0, 0),(100,100,100,100))
+
+#A and B are the distance from the top left corner. This places the image on the surface A px down and B px left.
+#C and D are the cropped part of the image from the top left corner. C px down, D px left.
+#E and F define the image size.
+#(A, B),(C,D,E,F))
+
+pawn = pygame.Surface((400,400))
+pawn.blit(texture_pieces, (0, 0),(0,0,400,400))
 
 
 objects = []
